5c450fa1f08a3002a50e70dc8a17a7713795ff45
gammyp <gamebep@gmail.com>
2018-08-04 12:01:22 +0700

    Create init xoModeltest.java

---
+++ Horizontalpok9/test/xoModeltest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.naming.spi.DirStateFactory;
+import javax.naming.spi.DirStateFactory.Result;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Game
+ */
+public class xoModeltest {
+    
+    @Test
+    public void testCreateUser() {
+        assertTrue(false);
+    }
+}
